1a803dbedae4dd47e8022caff8a9507531e479ec
imhansai <imhansai@foxmail.com>
2026-01-27 11:06:01 +0800

    测试调用工具

---
+++ src/main/kotlin/dev/fromnowon/embabellearn/record2/ToolShell.kt
@@ -0,0 +1,28 @@
+package dev.fromnowon.embabellearn.record2
+
+import com.embabel.agent.api.common.Ai
+import com.embabel.agent.core.CoreToolGroups
+import com.embabel.agent.tools.math.MathTools
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.shell.standard.ShellComponent
+import org.springframework.shell.standard.ShellMethod
+
+@ShellComponent
+class ToolShell {
+
+    @Autowired
+    private lateinit var mathToolGroup: MathTools
+
+    @Autowired
+    private lateinit var ai: Ai
+
+    @ShellMethod("数学计算")
+    fun tool(): String {
+        return ai
+            .withDefaultLlm()
+            // .withToolGroup(mathToolGroup)
+            .withToolGroup(CoreToolGroups.MATH)
+            .generateText("123 * 321 等于多少")
+    }
+
+}
